a92ae73fe796058506307867716656620607f5f8
maiziwjp <2558011040@qq.com>
2018-04-24 20:24:33 +0800

    狗狗日记

---
+++ script/data.js
@@ -0,0 +1,24 @@
+var itemData=[{
+img:0,
+emIndex:1,
+weIndex:1,
+piIndex:1,
+data:"2018-10-10",
+day:"星期五",
+title:"标题",
+diary:"内容"
+},
+{
+img:0,
+emIndex:1,
+weIndex:1,
+piIndex:1,
+data:"2018-10-10",
+day:"星期五",
+title:"标题",
+diary:"内容"
+}];
+var fj2=["../image/cloudy.png","../image/rain.png","../image/snow.png","../image/sunny.png"];
+var fj=["../image/fj1.jpg","../image/fj2.jpg","../image/fj3.jpg","../image/fj4.jpg"];
+//var emotion=["../image/happy.png","../image/rain.png","../image/snow.png","../image/sunny.png"];
+var fj1=["../image/happy.png","../image/sad.png","../image/normal.png"];
